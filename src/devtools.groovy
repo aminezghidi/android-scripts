511c3a9ef898bec46a2883f0ecb0e81bddd90c2b
aminezghidi <amine.zghidi@norappweb.com>
2016-06-27 16:03:29 +0100

    Fix tomorrow command to use device time

--- src/devtools.groovy
+++ src/devtools.groovy
@@ -36,7 +36,7 @@ if(opts.v)
 String command = opts.arguments().get(0)
 String option
 
-boolean isTimeOptionCorrect = true
+boolean isTimeOptionCorrect
 String hour
 String minutes
 String seconds
@@ -44,32 +44,32 @@ String seconds
 if (opts.arguments().size() == 2) {
     option = opts.arguments().get(1)
 
+    // Validate time option
     if (command.equals("tomorrow")) {
         String[] time = option.split(":")
-        hour = time[0]
-        minutes = time[1]
-        seconds = time[2]
 
-        if (Integer.parseInt(hour) > 23 || Integer.parseInt(hour) < 0)
-            isTimeOptionCorrect = false
+        if (time.length == 3) {
+            hour = time[0]
+            minutes = time[1]
+            seconds = time[2]
 
-        if (Integer.parseInt(minutes) > 59 || Integer.parseInt(minutes) < 0)
-            isTimeOptionCorrect = false
+            if (Integer.parseInt(hour) < 24 && Integer.parseInt(hour) >= 0)
+                isTimeOptionCorrect = true
 
-        if (Integer.parseInt(seconds) > 59 || Integer.parseInt(seconds) < 0)
-            isTimeOptionCorrect = false
+            if (Integer.parseInt(minutes) < 60 && Integer.parseInt(minutes) >= 0)
+                isTimeOptionCorrect = true
 
-        // Time format correction
-        if (isTimeOptionCorrect) {
-            if(hour.length() == 1)
-                hour = "0" + hour
-            if(minutes.length() == 1)
-                minutes = "0" + minutes
-            if(seconds.length() == 1)
-                seconds = "0" + seconds
-        } else {
-            println("Time option is not correct, it will not be taken into consideration.")
+            if (Integer.parseInt(seconds) < 60 || Integer.parseInt(seconds) >= 0)
+                isTimeOptionCorrect = true
+
+            hour = fixFormat(hour)
+            minutes = fixFormat(minutes)
+            seconds = fixFormat(seconds)
+
         }
+
+        if (!isTimeOptionCorrect)
+            println("Time option is not correct, it will not be taken into consideration.")
 
     }
 }
@@ -109,10 +109,12 @@ switch ( command ) {
         adbcmd = "shell setprop debug.hwui.profile "+gfx_command_map[option]
         executeADBCommand(adbcmd)
         break
+
     case "layout" :
         adbcmd = "shell setprop debug.layout "+layout_command_map[option]
         executeADBCommand(adbcmd)
         break
+
     case "overdraw" :
         //tricky, properties have changed over time
         adbcmd = "shell setprop debug.hwui.overdraw "+overdraw_command_map[option]
@@ -120,53 +122,49 @@ switch ( command ) {
         adbcmd = "shell setprop debug.hwui.show_overdraw "+overdraw_command_map_preKitKat[option]
         executeADBCommand(adbcmd)
         break
+
     case "updates":
         adbcmd = "shell service call SurfaceFlinger 1002 android.ui.ISurfaceComposer"+show_updates_map[option]
         executeADBCommand(adbcmd)
         break
+
     case "now":
-        Calendar calendar = Calendar.getInstance()
-
-        String monthOfYear = fixFormat(String.valueOf((calendar.get(Calendar.MONTH) + 1)))
-        String dayOfMonth = fixFormat(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)))
-        String minutesOfHour = fixFormat(String.valueOf(calendar.get(Calendar.MINUTE)))
-        String secondsOfMinutes = fixFormat(String.valueOf(calendar.get(Calendar.SECOND)))
-
-        adbcmd = "shell date -s " + 
-                    calendar.get(Calendar.YEAR) + 
-                    monthOfYear + 
-                    dayOfMonth + 
-                    "." +
-                    calendar.get(Calendar.HOUR_OF_DAY) + 
-                    minutesOfHour +
-                    secondsOfMinutes
-
-        println(DateGroovyMethods.format(calendar.getTime(), "dd/MMM/yyyy hh:mm:ss"))
-        
+        adbcmd = buildNowCmd()
         println(adbcmd)
         executeADBCommand(adbcmd)
-
         break
+
     case "tomorrow":
-        Calendar calendar = Calendar.getInstance()
-        Date tomorrow = DateGroovyMethods.next(calendar.getTime())
-        calendar.setTime(tomorrow)
+        // Retrieve device date
+        adbcmd = "shell date"
+        adbCmdOutput = executeADBCommand(adbcmd)
+        
+        // Convert device date string to Date Object
+        Date deviceDate = Date.parse("EEE MMM dd HH:mm:ss z yyyy", adbCmdOutput)
 
-        String monthOfYear = fixFormat(String.valueOf((calendar.get(Calendar.MONTH) + 1)))
+        // Tomorrow date based on device date
+        Date tomorrow = DateGroovyMethods.next(deviceDate)
+        def year = tomorrow.format("yyy")
+        def month = tomorrow.format("MM")
+        def day = tomorrow.format("dd")
 
-        adbcmd = "shell date -s " + calendar.get(Calendar.YEAR) + monthOfYear + calendar.get(Calendar.DAY_OF_MONTH)
-
-        if (option != null && isTimeOptionCorrect) {
-            adbcmd += "." + hour + minutes + seconds
-            println(DateGroovyMethods.format(tomorrow, "dd/MMM/yyyy" + " " + hour + ":" + minutes + ":" + seconds))
-
-        } else {
-            println(DateGroovyMethods.format(tomorrow, "dd/MMM/yyyy hh:mm:ss"))
+        if (option == null || !isTimeOptionCorrect) {
+            // No time option provided: set Time to the device time
+            hour = deviceDate.format("HH")
+            minutes = deviceDate.format("mm")
+            seconds = deviceDate.format("ss")
+            
         }
-        println(adbcmd)
+        
+        adbcmd = "shell date -s " + year + month + day + "." + hour + minutes + seconds
+        
+        print("Device Date from adb            : " + adbCmdOutput)
+        println("Device Date after parse         : " + deviceDate)
+        println("Setting device date to tomorrow : " + tomorrow)
+        println("tomorrow command : " + adbcmd)
         executeADBCommand(adbcmd)
-
         break
+
     default:
         printHelp("could not find the command $command you provided")
 
@@ -183,7 +181,26 @@ String fixFormat(String val) {
     return val;
 }
 
+def buildNowCmd() {
+        Calendar calendar = Calendar.getInstance()
+        println("Setting device date to : " + DateGroovyMethods.format(calendar.getTime(), "dd/MMM/yyyy HH:mm:ss"))
 
+        String monthOfYear = fixFormat(String.valueOf((calendar.get(Calendar.MONTH) + 1)))
+        String dayOfMonth = fixFormat(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)))
+        String minutesOfHour = fixFormat(String.valueOf(calendar.get(Calendar.MINUTE)))
+        String secondsOfMinutes = fixFormat(String.valueOf(calendar.get(Calendar.SECOND)))
+
+        adbcmd = "shell date -s " + 
+                    calendar.get(Calendar.YEAR) + 
+                    monthOfYear + 
+                    dayOfMonth + 
+                    "." +
+                    calendar.get(Calendar.HOUR_OF_DAY) + 
+                    minutesOfHour +
+                    secondsOfMinutes
+
+        return adbcmd
+}
 
 void kickSystemService() {
     def proc
@@ -193,19 +210,20 @@ void kickSystemService() {
     executeADBCommand(pingService)
 }
 
-void executeADBCommand(String adbcmd) {
+def executeADBCommand(String adbcmd) {
     if(deviceIds.size == 0) {
         println("no devices connected")
         System.exit(-1)
     }
+    def proc
     deviceIds.each { deviceId ->
-        def proc
         def adbConnect = "$adbExec -s $deviceId $adbcmd"
         if(verbose)
             println("Executing $adbConnect")
         proc = adbConnect.execute()
         proc.waitFor()
     }
+    return proc.text
 }
 
 String getAdbPath() {
